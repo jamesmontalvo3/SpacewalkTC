4a132b1d1c227da5f424529a3e8b4ed54ef742e5
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-04-25 17:51:46 -0500

    Figuring out how to handle multiple S/Ns and QTY > 1

--- SpacewalkTC.sql
+++ SpacewalkTC.sql
@@ -3,15 +3,22 @@ CREATE TABLE items_on_event (
 
 	-- For each event there may be many revs, and within each rev there will
 	-- be many items. Each item_number should be used only once within each
-	-- rev, though.
+	-- rev, though. Thus, the event_id/version/item_number combination is
+	-- unique.
 	event_id					int unsigned NOT NULL,
 	version						int unsigned NOT NULL,
 	item_number					int unsigned NOT NULL AUTO_INCREMENT,
 
-	-- If it's an IMS item populate these, else they should be null
+	-- If this is an IMS item: populate these; Else: they should be null.
 	-- Note: for each event_id/rev combo there should be only one 
 	-- ims_cage/ims_pn/ims_sn combo...but there can be multiple instances
 	-- of null ims_cage/pn/sn...so I'm not sure I can make an index for it
+	--
+	-- Should ims_sn have a "not yet set" value? perhaps NULL is not-yet-set
+	-- and an empty string is a blank S/N? That could be confusing...so 
+	-- maybe not-yet-set should be indicated by some special string like 
+	-- "SERIALNUMBER-NOTYETSET". Or perhaps another column is required to 
+	-- indicate serial number is TBD.
 	ims_cage					varchar(5),
 	ims_pn						varchar(32),
 	ims_sn						varchar(32),
@@ -21,16 +28,21 @@ CREATE TABLE items_on_event (
 	-- it is not a required field. Cage/pn/sn uniquely identify items in IMS.
 	ims_barcode					varchar(9),
 	
+	-- Determines whether or not to show the serial number in the tool gather
+	-- document. Since some items have nonsense serial numbers (e.g. common
+	-- items like wipes, wire ties and bolts) we need to be able to hide them.
+	show_sn						boolean default 1,
+	
 	-- Used to override the defaults in the `item_display_text` table and the
 	-- Ops Nom from IMS. 
-	
+	--
 	-- Also, if the item is not in IMS (a "custom item") then this is how it
 	-- will be identified.
 	display_text				varchar(255) NOT NULL default '',
 	
 	-- Used to identify which item will be the parent of this item (not the
 	-- current parent per IMS, but the intended parent per the tool config).
-
+	-- 
 	-- Note: The following columns were removed because parents may not always
 	-- be IMS items since we need to support "custom" items:
 	--   future_parent_cage
@@ -38,9 +50,20 @@ CREATE TABLE items_on_event (
 	--   future_parent_sn
 	future_parent_item_number 	int unsigned NOT NULL,
 	
-	-- Shit.
+	-- Shit. This is hard. Not sure if this is the best way but here we go:
+	-- Each item will have its own line in the database, but the 
+	-- merge_with_item_number will allow there to be line items in the tool
+	-- config that show qty>1 and have multiple serial numbers. One item will
+	-- be sort of the "master" item, and all others will have the 
+	-- merge_with_item_number set to that item. Additionally, the table
+	-- item_display_text has a field "allow_multiple_qty" which will allow
+	-- some items to set qty>1 without listing multiple S/Ns. For items with
+	-- this set to FALSE (zero) setting the qty>1 will automatically create
+	-- additional S/N fields.
 	-- @TODO: How do we handle multiple IMS S/Ns?
+	-- @TODO: probably should go with TINYINT or SMALLINT here...
 	qty 						int unsigned NOT NULL default 1,
+	merge_with_item_number		int unsigned,
 	
 	-- Displayed in the "final location" column, under the location code
 	-- as an optional note. For example it may say "EMU 3005" or "Mesh bag
@@ -81,7 +104,8 @@ CREATE TABLE item_display_text (
 	ims_pn						varchar(32) NOT NULL,
 	
 	-- text to display
-	display_text				varchar(255) NOT NULL default '' 
+	display_text				varchar(255) NOT NULL default '',
+	allow_multiple_qty			boolean default 0
 
 ) ENGINE=InnoDB, DEFAULT CHARSET=utf8;
 
--- planning/notes.md
+++ planning/notes.md
@@ -17,28 +17,22 @@ eosFieldValue.js notes
 Misc Notes
 ----------
 * Rest of page...what other form info?
-** what will the workflow be like?
+  * what will the workflow be like?
 * eosTree or aciTree with eosTree merged in
 * new backend, stores individual data items
 * Output:
-** Generate word document with 3-column tool config
-** Generate excel document (or PDF'd big table o'tools)
-*** Later perhaps generate IPV XML procedure?
+  * Generate word document with 3-column tool config
+  * Generate excel document (or PDF'd big table o'tools)
+    * Later perhaps generate IPV XML procedure?
 * IMS integration
 * Limited Life
 * Tie into OOT? Go for EVA
+  * when this is live, ISOs and IMS can adopt principle of putting something in "notes" for alerting SpacewalkTC that there is an issue with a particular item. Notes something like "This is some useless info !alert(This is some warning info)
 
 Big Picture Plan
 ----------------
 
-1. Create minimum viable product
-2. Publish to Github with cool name like SpacewalkTC (future procedure editor like SpacewalkPE)
-3. Request assistance from hacker news community
-
-### Create minimum viable product
-
 1. Determine data format
-	1. Save-able format
 	2. Server format
 	3. 
 2. Determine event and revision method
